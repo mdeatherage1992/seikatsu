3d5225e5042ee15e118a8881779f46e7c5765efb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-11-01 15:09:34 -0400

    grid and main components up

--- seikatsu/src/index.js
+++ seikatsu/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Grid extends Component {
+  render() {
+    return(
+      <div>
+      Grid
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
+  render() {
+    return (
+      <div>
+      <h1> Seikatsu </h1>
+      <Grid
+      />
+      <h2> Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Main />, document.getElementById('root'));
